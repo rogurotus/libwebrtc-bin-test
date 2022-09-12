bda9fa0046ff4ba257887b74b49439d627ae6949
[Bot] Instrumentisto <57999751+instrumentisto-bot@users.noreply.github.com>
2022-09-12 16:07:11 +0200

    Upgrade to 105.0.5195.102 version (#9)
    - remove `--bitcode` GN flag when building for iOS
    - upgrade patches to actual
    - fix Windows build
    
    Co-authored-by: rogurotus <rogurotus@gmail.com>

--- build.windows.ps1
+++ build.windows.ps1
@@ -117,6 +117,8 @@ Exec { gclient sync --with_branch_heads -r $WEBRTC_COMMIT }
 Exec { git apply --ignore-space-change -v $PATCH_DIR\add_licenses.patch }
 Exec { git apply --ignore-space-change -v $PATCH_DIR\webrtc_voice_engine.patch }
 Exec { git apply --ignore-space-change -v $PATCH_DIR\win_dynamic_crt.patch }
+Exec { git apply --ignore-space-change -v $PATCH_DIR\windows_fix_towupper.patch }
+Exec { git apply --ignore-space-change -v $PATCH_DIR\windows_fix_type_traits.patch }
 Pop-Location
 
 Get-PSDrive
