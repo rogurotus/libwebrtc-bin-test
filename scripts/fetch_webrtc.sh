0c2e71267bd25f17755bd13bb0576051ce2379fe
[Bot] Instrumentisto <57999751+instrumentisto-bot@users.noreply.github.com>
2023-01-18 13:06:00 +0100

    Upgrade to 109.0.5414.74 version (#26)
    Co-authored-by: alexlapa <lapa.alex@ex.ua>

--- scripts/fetch_webrtc.sh
+++ scripts/fetch_webrtc.sh
@@ -27,6 +27,11 @@ if [ -f "$WEBRTC_DIR/.gclient" ]; then
     git reset --hard;
     git clean -xdf;
   fi
+  if [ -d "$WEBRTC_DIR/src/buildtools" ]; then
+    cd "$WEBRTC_DIR/src/buildtools";
+    git reset --hard;
+    git clean -xdf;
+  fi
 else
   echo "Getting WebRTC...";
   rm -f "$DEPOT_TOOLS_DIR/metrics.cfg"
