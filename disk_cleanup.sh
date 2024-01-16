b21bd24e3d76720a8785377253d42b94895c7d68
rogurotus <rogurotus@gmail.com>
2024-01-16 11:36:57 +0300

    fix

--- disk_cleanup.sh
+++ disk_cleanup.sh
@@ -18,6 +18,3 @@ sudo rm -rf /opt/ghc
 sudo rm -rf /usr/local/lib/android
 
 df -h
-
-
-# mvn install:install-file -DgroupId=com.github.instrumentisto -DartifactId=libwebrtc-bin -Dversion=119.0.0.1 -Dfile=/home/human/Downloads/build/libwebrtc-android/aar/libwebrtc.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=./android/ -DcreateChecksum=true
