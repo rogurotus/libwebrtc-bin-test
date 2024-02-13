932d95cd7f392944bb3338ca715f46cb27320f49
rogurotus <rogurotus@gmail.com>
2024-02-13 13:08:09 +0300

    try

--- disk_cleanup.sh
+++ disk_cleanup.sh
@@ -4,6 +4,7 @@ df -h
 
 # Cache 済み Docker Image の削除
 docker rmi $(docker images -q -a)
+sudo docker image prune --all --force
 
 # Boost の削除
 sudo rm -rf /usr/local/share/boost
@@ -15,7 +16,7 @@ sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 
 # Removing large packages
-sudo apt-get remove -y 'php.*' aspnetcore-* dotnet-* firefox gcc libmysqlclient* mysql-* google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get remove -y 'php.*' '^mongodb-.*' '^aspnetcore-.*' '^dotnet-.*' firefox gcc libmysqlclient* '^mysql-.*' azure-cli google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri google-cloud-sdk google-cloud-cli
 sudo apt-get autoremove -y
 sudo apt-get clean
 
