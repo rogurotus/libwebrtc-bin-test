6ef7ab5b9420300edf09c63a35afc99e071ff0ae
rogurotus <rogurotus@gmail.com>
2024-02-16 20:33:28 +0300

    Enable `setCodecPreferences` for iOS (#80)
    Additionally:
    - split Android build CI jobs by targets
    
    Co-authored-by: evdokimovs <evdokimovs@protonmail.ch>
    Co-authored-by: Kai Ren <tyranron@gmail.com>

--- build.windows.ps1
+++ build.windows.ps1
@@ -125,11 +125,11 @@ Get-PSDrive
 
 Push-Location $WEBRTC_DIR\src
   # WebRTC Debugビルド x64
-  Exec { gn gen $BUILD_DIR\debug_x64 --args='treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true use_custom_libcxx=false' }
+  Exec { gn gen $BUILD_DIR\debug_x64 --args="treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true use_custom_libcxx=false" }
   Exec { ninja -C "$BUILD_DIR\debug_x64" }
 
   # WebRTC Releaseビルド x64
-  Exec { gn gen $BUILD_DIR\release_x64 --args='is_debug=false treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true strip_debug_info=true symbol_level=0 use_custom_libcxx=false' }
+  Exec { gn gen $BUILD_DIR\release_x64 --args="is_debug=false treat_warnings_as_errors=false rtc_use_h264=true rtc_include_tests=false rtc_build_tools=false rtc_build_examples=false is_component_build=false use_rtti=true strip_debug_info=true symbol_level=0 use_custom_libcxx=false" }
   Exec { ninja -C "$BUILD_DIR\release_x64" }
 Pop-Location
 
--- disk_cleanup.sh
+++ disk_cleanup.sh
@@ -4,6 +4,7 @@ df -h
 
 # Cache 済み Docker Image の削除
 docker rmi $(docker images -q -a)
+sudo docker image prune --all --force
 
 # Boost の削除
 sudo rm -rf /usr/local/share/boost
@@ -13,6 +14,11 @@ sudo rm -rf /usr/share/dotnet
 
 # Haskell の削除
 sudo rm -rf /opt/ghc
+
+# Removing large packages
+sudo apt-get remove -y 'php.*' '^mongodb-.*' '^aspnetcore-.*' '^dotnet-.*' firefox gcc libmysqlclient* '^mysql-.*' azure-cli google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri google-cloud-sdk google-cloud-cli
+sudo apt-get autoremove -y
+sudo apt-get clean
 
 # Android SDK の削除
 sudo rm -rf /usr/local/lib/android
